9606475e5149a500c74c11d817cfa46c063867ff
Petia2906 <petia.hrusanova@gmail.com>
2023-03-18 14:21:35 +0200

    classes Car and Customer

---
+++ Data/Model/Car.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Data.Model
+{
+    public class Car
+    {
+
+        public int Id { get; set; }
+        public string RegNo { get; set; }
+        public int Km { get; set; }
+        
+    }
+}
---
+++ Data/Model/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Car { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+
+
+    }
+}
