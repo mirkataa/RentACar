a97a15d84eecf0a0f6cf97cbb3ec2264a193e704
Petia2906 <petia.hrusanova@gmail.com>
2023-03-18 14:45:44 +0200

    data layer

---
+++ Data/Context.cs
@@ -0,0 +1,47 @@
+﻿using Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+/// <summary>
+/// Data Namespace
+/// </summary>
+namespace Data
+{
+    /// <summary>
+    /// ProductContext
+    /// </summary>
+    public class Context : DbContext
+    {
+        /// <summary>
+        /// Connection String
+        /// </summary> 
+        private const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=RentACar;Integrated Security=True;";
+
+        /// <summary>
+        /// Cars
+        /// </summary>
+        public DbSet<Car> Cars { get; set; }
+
+        /// <summary>
+        /// Customers
+        /// </summary>
+        public DbSet<Customer> Customers { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Context()
+        {
+            // Create the database automaticly
+            Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Connection string to Microsoft SQL Server
+        /// </summary> 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+    }
+}
