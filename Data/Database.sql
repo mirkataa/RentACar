78c86e3cf0f806e8090340d882e1b6b2c9bc330f
Mirena Koleva <mire.koleva@gmail.com>
2023-03-18 14:16:57 +0200

    Create Database

---
+++ Data/Database.sql
@@ -0,0 +1,22 @@
+﻿USE [RentACar];
+
+-- Create tables
+CREATE TABLE [Cars] 
+(
+	[Id] INT IDENTITY PRIMARY KEY NOT NULL, 
+	[RgNum] VARCHAR(50) NOT NULL, 
+	[Km] INT
+);
+
+CREATE TABLE [Customers] 
+(
+	[Id] INT IDENTITY PRIMARY KEY NOT NULL, 
+	[FirstName] VARCHAR(50) NOT NULL,
+	[LastName] VARCHAR(50) NOT NULL, 
+	[Phone] INT,
+	[Email] VARCHAR(50),
+	[CarId] INT,
+	CONSTRAINT [fk_customers_id_car] FOREIGN KEY([CarId]) REFERENCES [Cars]([Id]),
+	[From] DATETIME,
+	[To] DATETIME,
+);
