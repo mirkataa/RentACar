eda2d9be96dacd583ffc5bd24730a1b8e5aba404
511MK511 <mkacheva511@mail.bg>
2023-03-19 11:31:13 +0200

    Актуализация на коментарите за базата данни

--- Data/Model/Car.cs
+++ Data/Model/Car.cs
@@ -3,9 +3,17 @@ namespace Data.Model
 {
     public class Car
     {
-
+        /// <summary>
+        /// Идентификационен номер на кола
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Регистрационен номер на кола
+        /// </summary>
         public string RgNum { get; set; }
+        /// <summary>
+        /// Изминати километри от колата
+        /// </summary>
         public int Km { get; set; }
         
     }
--- Data/Model/Customer.cs
+++ Data/Model/Customer.cs
@@ -8,13 +8,37 @@ namespace Data.Model
 {
     public class Customer
     {
+        /// <summary>
+        /// Идентификационен номер на клиента
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Име на клиента
+        /// </summary>
         public string FirstName { get; set; }
+        /// <summary>
+        /// Фамилия на клиента
+        /// </summary>
         public string LastName { get; set; }
+        /// <summary>
+        /// Имейл адрес на клиента
+        /// </summary>
         public string Email { get; set; }
+        /// <summary>
+        /// Телефонен номер на клиента
+        /// </summary>
         public string Phone { get; set; }
+        /// <summary>
+        /// Наета кола от клиента
+        /// </summary>
         public int Car { get; set; }
+        /// <summary>
+        /// Начален час, в който колата е наета
+        /// </summary>
         public DateTime From { get; set; }
+        /// <summary>
+        /// Краен час, до който колата може да се ползва
+        /// </summary>
         public DateTime To { get; set; }
 
 
