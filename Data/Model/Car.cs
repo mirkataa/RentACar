2a41434262f242dd0517698ac39d5711134237bd
Mirena Koleva <mire.koleva@gmail.com>
2023-03-22 18:13:13 +0200

    Update Car Model

--- Data/Model/Car.cs
+++ Data/Model/Car.cs
@@ -1,4 +1,6 @@
-﻿/// <summary>
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+/// <summary>
 /// Пронстранство от имена на базата данни
 /// </summary>
 namespace Data.Model
@@ -13,16 +15,31 @@ namespace Data.Model
         /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Публично поле, отразяващо марката на колата
+        /// </summary>
+        [DisplayName("Марка")]
+        [Required(ErrorMessage = "Марка е задължително поле!")]
         public string Brand { get; set; }
+
         /// <summary>
         /// Публично поле, отразяващо регистрационния номер на колата
         /// </summary>
+        [DisplayName("Регистрационен номер")]
+        [Required(ErrorMessage = "Регистрационен номер е задължително поле!")]
         public string RgNum { get; set; }
+
         /// <summary>
         /// публично поле, отразяващо броя изминати километри от колата
         /// </summary>
+        [DisplayName("Изминати километри")]
+        [Required(ErrorMessage = "Изминати километри е задължително поле!")]
         public int Km { get; set; }
 
+        /// <summary>
+        /// Pублично поле, отразяващо дали колата е наета
+        /// </summary>
+        [DisplayName("Наета")]
         public bool IsRented { get; set; }
 
     }
