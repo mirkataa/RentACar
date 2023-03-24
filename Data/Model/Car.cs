c7f6d4adda89c47d0eabb844d346cf87b8599524
Mirena Koleva <mire.koleva@gmail.com>
2023-03-24 16:53:17 +0200

    Update Car Model: Override Equals

--- Data/Model/Car.cs
+++ Data/Model/Car.cs
@@ -42,5 +42,23 @@ namespace Data.Model
         [DisplayName("Наета")]
         public bool IsRented { get; set; }
 
+        /// <summary>
+        /// Преправяме метода да сравнява стойностите на пропъртитата на два обекта Car вместо техните референции
+        /// </summary>
+        /// <returns>Ако всички пропъртита имат еднакви стойности, връща true, в противен случай връща false.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Car other = (Car)obj;
+            return Id == other.Id
+                && Brand == other.Brand
+                && RgNum == other.RgNum
+                && Km == other.Km
+                && IsRented == other.IsRented;
+        }
     }
 }
