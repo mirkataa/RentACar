3d4e9c9073a17a469da82cde445f0a74cfa79fb8
Mirena Koleva <mire.koleva@gmail.com>
2023-03-25 15:58:16 +0200

    Update comments

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,43 +1,43 @@
 ﻿using Data.Model;
 using Microsoft.EntityFrameworkCore;
 
-/// <summary>
-/// Пронстранство от имена на базата данни
-/// </summary>
+///<summary>
+///Пронстранство от имена на базата данни
+///</summary>
 namespace Data
 {
-    /// <summary>
-    /// Публичен клас контекст, наследяващ
-    /// </summary>
+    ///<summary>
+    ///Публичен клас контекст, наследяващ
+    ///</summary>
     public class Context : DbContext
     {
-        /// <summary>
+        ///<summary>
         ///Настройка за връзка към базата данни
-        /// </summary> 
+        ///</summary> 
         private const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=RentACar;Integrated Security=True;";
 
-        /// <summary>
-        /// DbContext свойство на класа от данни за колите
-        /// </summary>
+        ///<summary>
+        ///DbContext свойство на класа от данни за колите
+        ///</summary>
         public DbSet<Car> Cars { get; set; }
 
-        /// <summary>
-        /// DbContext свойство на класа от данни за клиентите
-        /// </summary>
+        ///<summary>
+        ///DbContext свойство на класа от данни за клиентите
+        ///</summary>
         public DbSet<Customer> Customers { get; set; }
 
-        /// <summary>
-        /// Публичен конструктор на класа
-        /// </summary>
+        ///<summary>
+        ///Публичен конструктор на класа
+        ///</summary>
         public Context()
         {
-            // Автоматично създаване на базата данни
+            //Автоматично създаване на базата данни
             Database.EnsureCreated();
         }
 
-        /// <summary>
-        /// Свързване към Microsoft SQL Server
-        /// </summary> 
+        ///<summary>
+        ///Свързване към Microsoft SQL Server
+        ///</summary> 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString);
